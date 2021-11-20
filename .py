52ce5166708af120a68fb0bff92baecad53432ab
Mehmet <93699785+mehmetp01@users.noreply.github.com>
2021-11-20 13:14:16 +0300

    Add files via upload

---
+++ .py
@@ -0,0 +1,13 @@
+m = int(input("maddenin kütlesini giriniz (m)."))
+v = float(input("maddenin hızını (v) giriniz."))
+h = int(input("yerden yüksekliğini giriniz (h)."))
+g = 10
+ep = m*g*h
+ek = 1/2*m*v**2
+
+ep=m*9.8*h
+ek=m*v**2/2
+
+print(ep)
+print(ek)
+print("Mehmet PEZEN")
